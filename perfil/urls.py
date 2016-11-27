61ae303747b7a1e42bd165270eac844576f07644
Andres <andres@bancompara.mx>
2016-11-27 12:53:30 -0600

    Formas Crear Update delete

--- perfil/urls.py
+++ perfil/urls.py
@@ -1,13 +1,41 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 from perfil.views import *
 
 urlpatterns = [
+
+    # Pagina Principal
     url(r'^index', index),
-    url(r'^prueba', postPrueba),
-    url(r'^Registrar', FormularioRegistro),
-    url(r'^listar', CuentaList.as_view(), name='cuenta_listar'),
-    url(r'^nuevo', CuentaCreate.as_view(), name='cuenta_crear'),
-    url(r'^editar/(?P<pk>\d+)', CuentaUpdate.as_view(), name='cuenta_editar'),
-    url(r'^eliminar/(?P<pk>\d+)', CuentaDelete.as_view(), name='cuenta_eliminar'),
+
+    # CRUD Cuentas
+    url(r'^cuenta', postPrueba),
+    url(r'^cuenta/nueva', agregarCuenta),
+    url(r'^cuenta/editar/(?P<pk>\d+)', agregarCuenta),
+    url(r'^cuenta/eliminar/(?P<pk>\d+)', agregarCuenta),
+
+    # CRUD Tarjetas
+    url(r'^tarjeta', include([
+        url(r'^agregar/$', agregarTarjeta),
+        url(r'^modificar/$', agregarTarjeta),
+        url(r'^eliminar/(?P<pk>\d+)', agregarTarjeta),
+        url(r'^consultar/$', agregarTarjeta),
+    ])),
+
+    # CRUD Ingresos
+    url(r'^ingreso', include([
+        url(r'^agregar/$', agregarIngreso),
+        url(r'^modificar/$', agregarIngreso),
+        url(r'^eliminar/(?P<pk>\d+)', agregarIngreso),
+        url(r'^consultar/$', agregarIngreso),
+    ])),
+
+    # CRUD Egresos
+    url(r'^egreso', include([
+        url(r'^agregar/$', agregarEngreso),
+        url(r'^modificar/$', agregarEngreso),
+        url(r'^eliminar/(?P<pk>\d+)', agregarEngreso),
+        url(r'^consultar/$', agregarEngreso),
+    ])),
+
+
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,13 +1,6 @@
-
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-
-from .models import *
-from django.forms.models import model_to_dict
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from .forms import *
 from tokenapi.decorators import token_required
@@ -30,61 +23,16 @@ def index(request):
 
 
 @token_required
-def postPrueba(request):
-    if request.method == 'POST':
-        print "hola"
-    print "puto"
-    return HttpResponseRedirect('/convenios/')
+def agregarCuenta(request):
 
-@token_required
-def TarjetaRegistro(request):
     body = json.loads(request.body)
-    if request.method == 'POST':
-        username=body['username']
-        user = User.objects.get(username=username)
-        banco_nombre = body['banco']
-        banco = Banco.objects.get(nombre=banco_nombre)
-        numero_tarjeta = body['numero_tarjeta']
-        limite_credito = body['limite_credito']
-        tasa_interes = body['tasa_interes']
-        alias = body['alias']
-        fecha_corte = body['fecha_corte']
-        saldo = body['saldo']
-        tarjetaCredito = TarjetaCredito(
-            usuario=user,
-            banco=banco,
-            numero_tarjeta=numero_tarjeta,
-            limite_credito=limite_credito,
-            tasa_interes=tasa_interes,
-            alias=alias,
-            fecha_corte=fecha_corte,
-            saldo=saldo
-        )
-        tarjetaCredito.save()
-    else:
-        print ("no entro el post")
-        data = {'Success': False}
-
-    data = {'Success': True}
-    response = JsonResponse(data)
-    return response
-
-
-
-@token_required
-def FormularioRegistro(request):
-    #username = request.session['username']
-
-    #id_usuario = User.objects.get(username=username).id
-    #user = User.objects.get(username=username)
-
-    body=json.loads(request.body)
 
     if request.method == 'POST':
         print ("entro el post")
         username = body['username']
         user = User.objects.get(username=username)
         banco_nombre = body['banco']
+        banco = Banco.objects.get(nombre=banco_nombre)
         numero_cuenta = body['numero_cuenta']
         clabe = body['clabe']
         tipo_cuenta = body['tipo_cuenta']
@@ -94,123 +42,245 @@ def FormularioRegistro(request):
         tasa_inflacion = body['tasa_inflacion']
         plazo = body['plazo']
 
-        banco = Banco.objects.get(nombre=banco_nombre)
         cuenta = Cuenta(usuario=user,
-                                banco=banco,
-                                numero_cuenta=numero_cuenta,
-                                clabe=clabe,
-                                tipo_cuenta=tipo_cuenta,
-                                monto=monto,
-                                t_tarjeta_debito=t_tarjeta_debito,
-                                num_tarjeta=num_tarjeta,
-                                tasa_inflacion=tasa_inflacion,
-                                plazo=plazo)
+                        banco=banco,
+                        numero_cuenta=numero_cuenta,
+                        clabe=clabe,
+                        tipo_cuenta=tipo_cuenta,
+                        monto=monto,
+                        t_tarjeta_debito=t_tarjeta_debito,
+                        num_tarjeta=num_tarjeta,
+                        tasa_inflacion=tasa_inflacion,
+                        plazo=plazo)
         cuenta.save()
-
-
-        # create a form instance and populate it with data from the request:
-        # form = FormaCuenta(request.body)
-        # check whether it's valid:
-    #     if form.is_valid():
-    #         username = form.cleaned_data['usuario']
-    #         #id_usuario = User.objects.get(username=username).id
-    #         user = User.objects.get(username=username)
-    #         #form.users = request.user
-    #         # process the data in form.cleaned_data as required
-    #         # usuario = form.cleaned_data
-    #         banco = form.cleaned_data['banco']
-    #         numero_cuenta = form.cleaned_data['numero_cuenta']
-    #         clabe = form.cleaned_data['clabe']
-    #         tipo_cuenta = form.cleaned_data['tipo_cuenta']
-    #         monto = form.cleaned_data['monto']
-    #         t_tarjeta_debito = form.cleaned_data['t_tarjeta_debito']
-    #         num_tarjeta = form.cleaned_data['num_tarjeta']
-    #         tasa_inflacion = form.cleaned_data['tasa_inflacion']
-    #         plazo = form.cleaned_data['plazo']
-    #         form.save(commit=False)
-    #         cuenta = Cuenta(usuario=user,
-    #                         banco=banco,
-    #                         numero_cuenta=numero_cuenta,
-    #                         clabe=clabe,
-    #                         tipo_cuenta=tipo_cuenta,
-    #                         monto=monto,
-    #                         t_tarjeta_debito=t_tarjeta_debito,
-    #                         num_tarjeta=num_tarjeta,
-    #                         tasa_inflacion=tasa_inflacion,
-    #                         plazo=plazo)
-    #         cuenta.save()
-    #         print "hola"
-    #         # redirect to a new URL:
-    #         return HttpResponseRedirect('/convenios/')
-    #
-    #
-    # # if a GET (or any other method) we'll create a blank form
+        data = {'Success': True}
+    # if a GET (or any other method) we'll create a blank form
     else:
         print ("no entro el post")
-        #form = FormaCuenta()
-        data ={'Success': False}
+        data = {'Success': False}
 
+    response = JsonResponse(data)
+    return response
+
+
+@token_required
+def modificarCuenta(request, pk):
+    body = json.loads(request.body)
+
+    if request.method == 'POST':
+        banco_nombre = body['banco']
+        banco = Banco.objects.get(nombre=banco_nombre)
+        numero_cuenta = body['numero_cuenta']
+        clabe = body['clabe']
+        tipo_cuenta = body['tipo_cuenta']
+        monto = body['monto']
+        t_tarjeta_debito = body['t_tarjeta_debito']
+        num_tarjeta = body['num_tarjeta']
+        tasa_inflacion = body['tasa_inflacion']
+        plazo = body['plazo']
+
+    Cuenta.objects.filter(id=pk).update(banco=banco,
+                                        numero_cuenta=numero_cuenta,
+                                        clabe=clabe,
+                                        tipo_cuenta=tipo_cuenta,
+                                        monto=monto,
+                                        t_tarjeta_debito=t_tarjeta_debito,
+                                        num_tarjeta=num_tarjeta,
+                                        tasa_inflacion=tasa_inflacion,
+                                        plazo=plazo)
+
+
+@token_required
+def eliminarCuenta(request, pk):
+    cuenta = Cuenta.objects.get(pk=pk)
+    cuenta.delete()
     data = {'Success': True}
     response = JsonResponse(data)
     return response
 
 
-class CuentaList(ListView):
-    model = Cuenta
-    template_name = 'cuenta_list.html'
+@token_required
+def agregarTarjeta(request):
+    body = json.loads(request.body)
+    if request.method == 'POST':
+        username = body['username']
+        user = User.objects.get(username=username)
+        banco_nombre = body['banco']
+        banco = Banco.objects.get(nombre=banco_nombre)
+        numero_tarjeta = body['numero_tarjeta']
+        limite_credito = body['limite_credito']
+        tasa_interes = body['tasa_interes']
+        alias = body['alias']
+        fecha_corte = body['fecha_corte']
+        saldo = body['saldo']
+        tarjetaCredito = TarjetaCredito(usuario=user,
+                                        banco=banco,
+                                        numero_tarjeta=numero_tarjeta,
+                                        limite_credito=limite_credito,
+                                        tasa_interes=tasa_interes,
+                                        alias=alias,
+                                        fecha_corte=fecha_corte,
+                                        saldo=saldo
+                                        )
+        tarjetaCredito.save()
+        data = {'Success': True}
+    else:
+        print ("no entro el post")
+        data = {'Success': False}
+
+    response = JsonResponse(data)
+    return response
 
 
-class CuentaCreate(CreateView):
-    model = Cuenta
-    template_name = 'cuenta_form.html'
-    form_class = FormaCuenta
-    success_url = reverse_lazy('perfil:cuenta_listar')
-
-    def get_context_data(self, **kwargs):
-        context = super(CuentaCreate, self).get_context_data(**kwargs)
-        if 'form' not in context:
-            context['form'] = self.form_class(self.request.GET)
-        return context
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            cuenta = form.save()
-            cuenta.save()
-            return HttpResponseRedirect(self.get_success_url())
-        else:
-            return self.render_to_response(self.get_context_data(form=form))
+@token_required
+def eliminarTrajeta(request, pk):
+    tarjeta = TarjetaCredito.objects.get(pk=pk)
+    tarjeta.delete()
+    data = {'Success': True}
+    response = JsonResponse(data)
+    return response
 
 
-class CuentaUpdate(UpdateView):
-    model = Cuenta
-    template_name = 'cuenta_form.html'
-    form_class = FormaCuenta
-    success_url = reverse_lazy('perfil:cuenta_listar')
+@token_required
+def agregarIngreso(request):
+    body = json.loads(request.body)
+    if request.method == 'POST':
+        username = body['username']
+        user = User.objects.get(username=username)
+        cuenta = body['cuenta']
+        nombre = body['nombre']
+        monto = body['monto']
+        recurrencia = body['recurrencia']
+        fijo = body['fijo']
+        tipo = body['tipo']
+        fecha = body['fecha']
+        ingreso = Ingreso(usuario=user,
+                          cuenta=cuenta,
+                          nombre=nombre,
+                          monto=monto,
+                          recurrencia=recurrencia,
+                          fijo=fijo,
+                          tipo=tipo,
+                          fecha=fecha)
+        ingreso.save()
+        data = {'Success': True}
+    else:
+        print ("no entro el post")
+        data = {'Success': False}
 
-    def get_context_data(self, **kwargs):
-        context = super(CuentaUpdate, self).get_context_data(**kwargs)
-        pk = self.kwargs.get('pk', 0)
-        cuenta = self.model.objects.get(id=pk)
-        if 'form' not in context:
-            context['form'] = self.form_class()
-        context['id'] = pk
-        return context
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object
-        id_cuenta = kwargs['pk']
-        cuenta = self.model.objects.get(id=id_cuenta)
-        form = self.form_class(request.POST, instance=cuenta)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(self.get_success_url())
-        else:
-            return HttpResponseRedirect(self.get_success_url())
+    response = JsonResponse(data)
+    return response
 
 
-class CuentaDelete(DeleteView):
-    model = Cuenta
-    template_name = 'cuenta_delete.html'
-    success_url = reverse_lazy('perfil:cuenta_listar')
+@token_required
+def eliminarIngreso(request, pk):
+    ingreso = Ingreso.objects.get(pk=pk)
+    ingreso.delete()
+    data = {'Success': True}
+    response = JsonResponse(data)
+    return response
+
+
+@token_required
+def agregarEngreso(request):
+    body = json.loads(request.body)
+    if request.method == 'POST':
+        username = body['username']
+        user = User.objects.get(username=username)
+        numero_tarjeta = body['tarjeta']
+        tarjeta = TarjetaCredito.objects.get(numero_tarjeta=numero_tarjeta)
+        nombre = body['nombre']
+        monto = body['monto']
+        recurrencia = body['recurrencia']
+        fijo = body['fijo']
+        tipo = body['tipo']
+        fecha = body['fecha']
+        financiado = body['financiado']
+        plazo = body['plazo']
+        ingreso = Ingreso(usuario=user,
+                          tarjeta=tarjeta,
+                          nombre=nombre,
+                          monto=monto,
+                          recurrencia=recurrencia,
+                          fijo=fijo,
+                          tipo=tipo,
+                          fecha=fecha,
+                          financiado=financiado,
+                          plazo=plazo)
+        ingreso.save()
+        data = {'Success': True}
+    else:
+        print ("no entro el post")
+        data = {'Success': False}
+
+    response = JsonResponse(data)
+    return response
+
+
+@token_required
+def eliminarEngreso(request, pk):
+    Engreso = Egreso.objects.get(pk=pk)
+    Engreso.delete()
+    data = {'Success': True}
+    response = JsonResponse(data)
+    return response
+
+
+@token_required
+def postPrueba(request):
+    if request.method == 'POST':
+        print "hola"
+    print "puto"
+    return HttpResponseRedirect('/convenios/')
+
+# @login_required()
+# def agregarCuenta(request):
+#     username = request.session['username']
+#     id_usuario = User.objects.get(username=username).id
+#     user = User.objects.get(username=username)
+#     if request.method == 'POST':
+#         create a form instance and populate it with data from the request:
+#         form = FormaCuenta(request.body)
+#         check whether it's valid:
+#         if form.is_valid():
+#             username = form.cleaned_data['usuario']
+#             #id_usuario = User.objects.get(username=username).id
+#             user = User.objects.get(username=username)
+#             #form.users = request.user
+#             # process the data in form.cleaned_data as required
+#             # usuario = form.cleaned_data
+#             banco = form.cleaned_data['banco']
+#             numero_cuenta = form.cleaned_data['numero_cuenta']
+#             clabe = form.cleaned_data['clabe']
+#             tipo_cuenta = form.cleaned_data['tipo_cuenta']
+#             monto = form.cleaned_data['monto']
+#             t_tarjeta_debito = form.cleaned_data['t_tarjeta_debito']
+#             num_tarjeta = form.cleaned_data['num_tarjeta']
+#             tasa_inflacion = form.cleaned_data['tasa_inflacion']
+#             plazo = form.cleaned_data['plazo']
+#             form.save(commit=False)
+#             cuenta = Cuenta(usuario=user,
+#                             banco=banco,
+#                             numero_cuenta=numero_cuenta,
+#                             clabe=clabe,
+#                             tipo_cuenta=tipo_cuenta,
+#                             monto=monto,
+#                             t_tarjeta_debito=t_tarjeta_debito,
+#                             num_tarjeta=num_tarjeta,
+#                             tasa_inflacion=tasa_inflacion,
+#                             plazo=plazo)
+#             cuenta.save()
+#             print "hola"
+#             # redirect to a new URL:
+#             return HttpResponseRedirect('/convenios/')
+#
+#
+#     # if a GET (or any other method) we'll create a blank form
+#     else:
+#         print ("no entro el post")
+#         # form = FormaCuenta()
+#         data = {'Success': False}
+#
+#     data = {'Success': True}
+#     response = JsonResponse(data)
+#     return response
